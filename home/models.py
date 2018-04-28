593cb50ff7d0d635093a6f201141f3f01b1072c7
Alex Martin <alexm118@vt.edu>
2018-04-28 12:47:56 -0400

    Adding BasePageWithHero (#4)
    Adding BasePageWithHero

--- home/models.py
+++ home/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 
 from wagtail.core.models import Page
-
+from wagtail.core.blocks import StructBlock, CharBlock
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
 
 class HomePage(Page):
     pass
+
+class HeroBlock(StructBlock):
+    subject = CharBlock()
+    byline = CharBlock(required=False)
+
+    class Meta:
+        icon = "user"
+        template = "home/blocks/hero.html"
+
+class BasePageWithHero(Page):
+    hero = StreamField([
+        ('hero', HeroBlock())
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('hero')
+    ]
+
+    class Meta:
+        abstract = True
