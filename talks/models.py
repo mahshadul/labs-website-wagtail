e44d5fb0d67132e427e7ba4ae5b14a33fb116390
Kevin Ellis <kevin.ellis@excella.com>
2018-04-28 14:03:36 -0400

    Talk detail (#10)
    Talk View

--- talks/models.py
+++ talks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+from home.models import BasePageWithHero
+
+
+class Talk(BasePageWithHero):
+
+    # Database fields
+
+    author = models.CharField(max_length=200)
+    body = RichTextField()
+    content_panels = BasePageWithHero.content_panels + [
+        FieldPanel('author', classname='full'),
+        FieldPanel('body', classname='full')
+    ]
