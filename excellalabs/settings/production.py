a15199b72ca183a26fb1cb2a0fae7e9e24f27179
Nick Oki <nick.oki@live.com>
2018-05-14 14:16:45 -0700

    Move AWS env vars from base to dev

--- excellalabs/settings/production.py
+++ excellalabs/settings/production.py
@@ -4,6 +4,3 @@ DEBUG = False
 
 SECRET_KEY = get_environment_variable('SECRET_KEY')
 ALLOWED_HOSTS = ['excellalabs.com']
-
-MEDIA_URL = 'https://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
