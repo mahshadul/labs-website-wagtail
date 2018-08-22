a791e7307770d6d0df8f71d2c415daaaf10ae573
walter <walterc@vt.edu>
2018-08-22 11:06:54 -0400

    homepage banner and project mini styling

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
 from wagtail.core.blocks import RichTextBlock
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from home.models import BasePageWithHero, BasePageWithBody
 
@@ -17,10 +18,20 @@ class ProjectPage(BasePageWithHero, BasePageWithBody):
 
     subtitle = models.CharField(max_length=200, blank=True)
     exerpt = RichTextField(help_text="Summary for list view, not displayed in detail view")
+    date = models.DateField()
+    graphic = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
     
     content_panels = BasePageWithHero.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('exerpt'),
+        FieldPanel('date'),
+        ImageChooserPanel('graphic'),
     ]
 
     parent_page_types = ['projects.ProjectList']
